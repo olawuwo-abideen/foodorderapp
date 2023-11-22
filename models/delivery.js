c2da0cf29f94d010b1b0b58bbe3f09b0cc7ce17b
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-11-22 15:48:08 +0100

    update models

--- models/delivery.js
+++ models/delivery.js
@@ -1,1 +1,48 @@
 const mongoose = require('mongoose');
+
+
+const deliverySchema = new mongoose.Schema(
+    {
+        email: {
+          type: String,
+          required: true,
+          maxlength: 50,
+        },
+        password: {
+          type: String,
+          required: true,
+          maxlength: 50,
+        },
+        firstName: {
+          type: Number,
+          required: true,
+          maxlength: 30,
+          unique: true,
+        },
+        lastName: {
+            type: String,
+            required: true,
+            maxlength: 50,
+          },
+        address: {
+            type: String,
+            required: true,
+            maxlength: 50,
+          },
+        phoneNumber: {
+            type: Number,
+            required: true,
+            maxlength: 100,
+          },
+        isAvailable: {
+            type: Boolean,
+            required: true,
+          },
+      },
+)
+
+
+
+
+
+module.exports = mongoose.model('delivery', deliverySchema);
--- models/vendor.js
+++ models/vendor.js
@@ -1,1 +1,60 @@
 const mongoose = require('mongoose');
+
+
+const vendorSchema = new mongoose.Schema(
+    {
+        name: {
+          type: String,
+          required: true,
+          maxlength: 50,
+        },
+        ownerName: {
+          type: String,
+          required: true,
+          maxlength: 50,
+        },
+        foodType: {
+          type: Number,
+          required: true,
+          maxlength: 30,
+          unique: true,
+        },
+        address: {
+            type: String,
+            required: true,
+            maxlength: 50,
+          },
+        phone: {
+            type: String,
+            required: true,
+            maxlength: 50,
+          },
+        email: {
+            type: Number,
+            required: true,
+            maxlength: 100,
+          },
+        password: {
+            type: Number,
+            required: true,
+          },
+        serviceAvailable: {
+            type: Number,
+            required: true,
+          },
+        rating: {
+            type: Number,
+            required: true,
+          },
+        foods: [{
+            type: mongoose.SchemaTypes.ObjectId,
+            ref: 'food'
+        }],
+      },
+)
+
+
+
+
+
+module.exports = mongoose.model('vendor', vendorSchema);
