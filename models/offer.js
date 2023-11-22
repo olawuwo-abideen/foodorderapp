b8cc9fd6e7626549c527e5be94cd29314462c083
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-11-22 16:35:42 +0100

    update models

--- models/offer.js
+++ models/offer.js
@@ -1,3 +1,55 @@
 const mongoose = require('mongoose');
 
 
+const offerSchema = new mongoose.Schema(
+    {
+        orderId: {
+          type: String,
+          required: true,
+          maxlength: 50,
+        },
+        vendorId: {
+          type: String,
+          required: true,
+          maxlength: 50,
+        },
+        items: [
+            {
+                food: {type: Schema.Types.ObjectId, ref: "food", require: true},
+                unit: { type: Number, require: true}
+            }
+        ],
+        paidAmount: {
+          type: Number,
+          required: true,
+        },
+        orderDate: {
+            type: String,
+            required: true,
+            maxlength: 50,
+          },
+          orderStatus: {
+            type: String,
+            required: true,
+          },
+          remarks: {
+            type: String,
+            required: true,
+          },
+        deliveryId: {
+            type: String,
+            required: true,
+          },
+        readyTime: {
+            type: Number,
+            required: true,
+          }
+      },
+)
+
+
+
+
+
+module.exports = mongoose.model('offer', offerSchema);
+
