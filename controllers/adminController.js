8cff4b7ef693812436cbe2e967e07b0f1a6f3cfe
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-11-23 09:39:29 +0100

    update admin controllers

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,9 +1,60 @@
-
+const Delivery = require('../models/delivery')
+const Transaction = require('../models/vendor')
+const Vendor = require('../models/vendor')
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
 
 
 
-const createProduct = async (req, res) => {
+const createVendor = async (req, res) => {
     
   };
+
+  const  getSingleVendor = async (req, res) => {
+    
+  };
+
+
+const getAllVendors = async (req, res) => {
+    
+  };
+
+
+  const  getAllTransactions = async (req, res) => {
+    
+  };
+
+
+
+
+  const  getSingleTransaction = async (req, res) => {
+    
+  };
+
+
+
+
+  
+  const  verifyDeliveryUser = async (req, res) => {
+    
+  };
+
+
+
+  
+  const  getDeliveryUsers = async (req, res) => {
+    
+  };
+
+
+  module.exports = {
+    createVendor,
+    getSingleVendor,
+    getAllVendors,
+    getAllTransactions,
+    getSingleTransaction,
+    verifyDeliveryUser,
+    getDeliveryUsers
+
+  };
+  
