8c4f496f13266faf11b24d7681ef800e628071a9
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-11-23 18:09:29 +0100

    update routes

--- routes/customer.js
+++ routes/customer.js
@@ -7,8 +7,6 @@ const {
     login ,
     getCustomerProfile,
     updateCustomerProfile,
-    assignOrderForDelivery,
-    validateTransaction,
     createOrder,
     getSingleOrder,
     getAllOrders,
@@ -18,3 +16,28 @@ const {
     verifyOffer,
     createPayment
 } = require ('../controllers/customerController')
+
+
+
+
+
+router.post('/register', register)
+
+router.post('/login', login)
+
+
+
+router.get('/profile', getCustomerProfile)
+router.patch('/profile', updateCustomerProfile)
+
+router.post('/cart', addToCart)
+router.get('/cart', getCart)
+router.delete('/cart', deleteCart)
+
+router.get('/offer/verify/:id', verifyOffer);
+
+router.post('/create-payment', createPayment);
+
+router.post('/create-order', createOrder);
+router.get('/orders', getAllOrders);
+router.get('/order/:id', getSingleOrder)
