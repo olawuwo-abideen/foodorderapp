cfd2653d7e977185c13f8b5a121d4db85ce1f173
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-11-23 17:16:31 +0100

    update routes

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -8,3 +8,102 @@ const Transaction = require('../models/transaction')
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
 
+
+
+
+const register = async (req, res) => {
+    
+};
+
+const  login = async (req, res) => {
+  
+};
+
+
+const getCustomerProfile = async (req, res) => {
+  
+};
+
+
+const  updateCustomerProfile = async (req, res) => {
+  
+};
+
+
+
+
+const  assignOrderForDelivery = async (req, res) => {
+  
+};
+
+const validateTransaction = async (req, res) => {
+    
+};
+
+const  createOrder = async (req, res) => {
+  
+};
+
+
+const getSingleOrder = async (req, res) => {
+  
+};
+
+
+const  getAllOrders = async (req, res) => {
+  
+};
+
+
+
+
+const  addToCart = async (req, res) => {
+  
+};
+
+
+
+
+const getCart = async (req, res) => {
+  
+};
+
+
+const  deleteCart = async (req, res) => {
+  
+};
+
+
+
+
+const  verifyOffer = async (req, res) => {
+  
+};
+
+
+
+
+const  createPayment = async (req, res) => {
+  
+};
+
+
+
+
+
+module.exports = {
+    register,
+    login ,
+    getCustomerProfile,
+    updateCustomerProfile,
+    assignOrderForDelivery,
+    validateTransaction,
+    createOrder,
+    getSingleOrder,
+    getAllOrders,
+    addToCart,
+    getCart,
+    deleteCart,
+    verifyOffer,
+    createPayment
+}
--- routes/customer.js
+++ routes/customer.js
@@ -1,2 +1,20 @@
 const express = require('express');
 const router = express.Router();
+
+
+const {
+    register,
+    login ,
+    getCustomerProfile,
+    updateCustomerProfile,
+    assignOrderForDelivery,
+    validateTransaction,
+    createOrder,
+    getSingleOrder,
+    getAllOrders,
+    addToCart,
+    getCart,
+    deleteCart,
+    verifyOffer,
+    createPayment
+} = require ('../controllers/customerController')
