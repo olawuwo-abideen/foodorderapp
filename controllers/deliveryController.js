9186dee320bb6c2484dd05dafa31ff6f74daca9d
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-11-23 14:26:44 +0100

    update routes

--- controllers/deliveryController.js
+++ controllers/deliveryController.js
@@ -1,4 +1,10 @@
-
-
+const Customer = require('../models/customer')
+const Delivery = require('../models/delivery')
+const Food = require('../models/food')
+const Vendor = require('../models/vendor')
+const Offer = require('../models/offer')
+const Order = require('../models/order')
+const Transaction = require('../models/transaction')
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
+
