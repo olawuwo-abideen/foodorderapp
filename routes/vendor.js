eaed60328942f2d96806580fc69c4eeb14232446
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-11-23 15:01:20 +0100

    update routes

--- routes/vendor.js
+++ routes/vendor.js
@@ -19,4 +19,24 @@ const {
 
 
 
+router.get('/login', vendorLogin);
+
+router.get('/profile', getVendorProfile);
+router.patch('/profile', updateVendorProfile);
+router.patch('/service', updateVendorService);
+
+router.post('/food', addSingleFood);
+router.get('/food', getAllFoods)
+
+
+router.get('/orders', getCurrentOrder);
+router.put('/order/:id/process', processOrder);
+router.get('/order/:id', getOrderDetails)
+ 
+
+//Offers
+router.get('/offers', getAllOffers);
+router.post('/offer', addSingleOffer);
+router.put('/offer/:id', editSingleOffer)
+
 
