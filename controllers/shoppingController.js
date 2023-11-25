493ed213fb4a9f82aeaaad6c414107fd926ed15a
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-11-25 13:12:10 +0100

    update controllers

--- controllers/shoppingController.js
+++ controllers/shoppingController.js
@@ -1,19 +1,31 @@
 const Offer = require('../models/offer')
 const Vendor = require('../models/vendor')
-
-
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
 
 
  
 const  getAvailableFood = async (req, res) => {
-    
+   
+    const food = await Vendor.find({serviceAvailable: true}).sort([['rating', 'descending']]).populate('foods')
+
+    if(food.length > 0){
+         return res.status(StatusCodes.OK).json({food})
+    }
+    return res.status(StatusCodes.NOT_FOUND).json({ msg: 'No Food is  available at the moment!'});
 };
 
 
 const  getTopRestaurants = async (req, res) => {
     
+    const toprestaurant = await Vendor.find({ serviceAvailable: true}).sort([['rating', 'descending']]).limit(10)
+
+    if(toprestaurant.length > 0){
+        return res.status(StatusCodes.OK).json({toprestaurant})
+    }
+    
+    return res.status(StatusCodes.NOT_FOUND).json({ msg: 'no data at the moment!'});
+
 };
 
 const  searchFoods = async (req, res) => {
