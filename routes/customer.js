189c4bbbff0ac219fa332ab4e0efe6a86357793c
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-11-29 18:45:39 +0100

    routes and app.js

--- routes/customer.js
+++ routes/customer.js
@@ -3,38 +3,38 @@ const router = express.Router();
 
 
 const {
-    register,
-    login ,
+    customerSignup,
+    customerLogin ,
+    customerLogout,
+    updateCustomerPassword,
     getCustomerProfile,
     updateCustomerProfile,
+    assignOrderForDelivery,
+    validateTransaction,
     createOrder,
     getSingleOrder,
     getAllOrders,
-    addToCart,
-    getCart,
-    deleteCart,
-    verifyOffer,
-    createPayment
 } = require ('../controllers/customerController')
 
 
 
 
 
-router.post('/register', register)
+router.post('/register', customerSignup)
 
-router.post('/login', login)
+router.post('/login', customerLogin)
+
+router.get('/logout', customerLogout);
+
+router.patch('/updatepassword', updateCustomerPassword)
 
 router.get('/profile', getCustomerProfile)
 router.patch('/profile', updateCustomerProfile)
 
-router.post('/cart', addToCart)
-router.get('/cart', getCart)
-router.delete('/cart', deleteCart)
+router.post('/order/delivery', assignOrderForDelivery)
 
-router.get('/offer/verify/:id', verifyOffer);
+router.get('/verify/transaction', validateTransaction);
 
-router.post('/create-payment', createPayment);
 
 router.post('/create-order', createOrder);
 router.get('/orders', getAllOrders);
